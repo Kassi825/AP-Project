f61fd16225d7932abcee62d8c8241098445432c7
Kassi825 <119680569+Kassi825@users.noreply.github.com>
2024-01-31 11:03:20 +0330

    Update Main.java

--- Project/src/main/java/Main.java
+++ Project/src/main/java/Main.java
@@ -1,17 +1,25 @@
-import java.io.*;
 import java.util.regex.Matcher;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        // this is when program starts
         Database database = new Database();
         Controller controller = new Controller();
         controller.setDatabase(database);
-        System.out.println("Please enter your username and password:");
+        String result;
+        // before we go to the menus we register the admin of the program
+        System.out.println("welcome to the app! you are the admin of the program. please enter your username and password:");
+        while (true) {
         String command = controller.getScanner().nextLine();
         Matcher matcher;
         if ((matcher = controller.getMatcher(command, "\\s*(?<username>\\S+)\\s+(?<password>\\S+)\\s*")) != null) {
-            controller.register(matcher.group("username"), matcher.group("password"), "SnappfoodAdmin");
+            result = controller.register(matcher.group("username"), matcher.group("password"), "SnappfoodAdmin");
+            System.out.println(result);
+            if (result.equals("register successful!"))
+                break;
+        }
+        else System.out.println("invalid command");
         }
         controller.setDatabase(database);
         controller.run();
